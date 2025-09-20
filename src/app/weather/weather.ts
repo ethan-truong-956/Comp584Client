49fbf948e1234280cf10fe531adfba7a9746f7f6
Truong <ethan.truong.956@my.csun.edu>
2025-09-19 17:35:24 -0700

    9/19/25 Update Established Table for weather data, imported styling and basic modules

--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -1,17 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { WeatherData } from '../weather-data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-weather',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './weather.html',
   styleUrl: './weather.scss'
 })
 export class Weather {
+forecast: WeatherData[] = [];
   constructor(http: HttpClient) {
     http.get<WeatherData[]>('http://localhost:5093/weatherforecast').subscribe(result => {
-      
+      this.forecast = result;
     });
   }
 }
