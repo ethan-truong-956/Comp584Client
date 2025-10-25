593e9ddbcb9b44985d95c7577d6fb03e3a7b7df8
Truong <ethan.truong.956@my.csun.edu>
2025-10-24 18:13:23 -0700

    10/24/25 Update

--- src/app/country-population/country-population.ts
+++ src/app/country-population/country-population.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PopulationData } from './population-data';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment.development';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-country-population',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './country-population.html',
   styleUrl: './country-population.scss'
 })
-export class CountryPopulation {
+
+export class CountryPopulation implements OnInit {
  population!: PopulationData;
-  constructor(http: HttpClient) {
-    http.get<PopulationData>(environment.apiUrl + "api/Countries/population/").subscribe(result => {
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
+
+  }
+  ngOnInit(): void {
+    let idparam = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get<PopulationData>(`${environment.apiUrl}api/Countries/population/${idparam}`).subscribe(result => {
       this.population = result;
-    });
+  });
   }
 }
